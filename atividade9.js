8954029ebd22bee6b7e2ca6f6d7b808787962dba
Cauã Tupinambá <cauatupinamba@gmail.com>
2025-04-25 11:12:32 -0300

    Use .length para contar 🎯

---
+++ atividade9.js
@@ -0,0 +1,11 @@
+let lista = ['Banana', 'Tomate', 'Pera', 'Maçã', 'Melancia', 'Pitaia']
+
+let quantidadedeFrutas = lista.length;
+
+// console.log('lista');
+console.log(`nossa lista tem ${quantidadedeFrutas} frutas`);
+console.log('-----------------------------------------')
+
+for (let i = 0; i < quantidadedeFrutas; i++) {
+    console.log(lista[i]);
+}
